7306c6531516d1565dc537efe9a46a13f01d18a3
Eli Pousson <eli.pousson@gmail.com>
2022-03-07 11:03:10 -0500

    feat: Add gt_geom_sf_rows + tbl_photo_key
    - feat: Add gt functions: gt_geom_sf_rows + tbl_photo_key
    - feat: Add magrittr pipe export
    - refactor: Add imported packages

---
+++ R/gt_geom_sf_rows.R
@@ -0,0 +1,111 @@
+#' Add maps of simple features into rows of a `gt` table
+#'
+#' Add maps of simple features into rows of a `gt` table with {ggplot2}. Function naming convention borrowed from {gtExtras} <https://jthomasmock.github.io/gtExtras/reference/gt_img_rows.html>
+#'
+#' @param gt_object An existing gt table object of class gt_tbl. The gt_object must be convertible to an sf object with sf::st_as_sf so do not drop the geometry before creating an sf object.
+#' @param mapping aesthetic mapping passed to layer_location_data, Default: NULL
+#' @param fill Fill (used for polygon geometry), Default: NA
+#' @param color Color (used for polygon, point, and line geometry), Default: 'black'
+#' @param size Color (used for polygon, point, and line geometry), Default: 1
+#' @param linetype Color (used for polygon and line geometry), Default: 'solid'
+#' @param height The absolute height (px) of the map in the table cell
+#' @param asp The aspect ratio of the map in the table cell.
+#' @param fn Optional rowwise function that results in ggplot2 map the replaces geometry column.
+#' @family gt
+#' @export
+#' @importFrom tibble rowid_to_column
+#' @importFrom sf st_as_sf st_geometry_type
+#' @importFrom dplyr group_nest group_by
+#' @importFrom ggplot2 ggplot
+#' @importFrom overedge layer_location_data layer_neatline
+#' @importFrom rlang as_function
+#' @importFrom purrr map
+#' @importFrom gt text_transform cells_body ggplot_image
+#' @importFrom tidyselect all_of
+gt_geom_sf_rows <-
+  function(gt_object,
+           mapping = NULL,
+           fill = NA,
+           color = "black",
+           size = 1,
+           linetype = "solid",
+           height = 100,
+           asp = 1,
+           fn = NULL,
+           ...) {
+    sf_object <-
+      tibble::rowid_to_column(
+        sf::st_as_sf(gt_object[["_data"]])
+      )
+
+    geom_col <-
+      attr(sf_object, "sf_column")
+
+    geom_type <-
+      sf::st_geometry_type(sf_object)
+
+    sf_nested <-
+      dplyr::group_nest(
+        dplyr::group_by(sf_object, .data[["rowid"]]),
+        keep = TRUE
+      )
+
+    if (is.null(fn)) {
+      if (all(sapply(geom_type, function(x) {x %in% c("POLYGON", "MULTIPOLYGON")}))) {
+        fn <- ~ ggplot2::ggplot() +
+          overedge::layer_location_data(
+            data = .x,
+            mapping = mapping,
+            fill = fill,
+            color = color,
+            size = size,
+            linetype = linetype,
+            ...
+          ) +
+          overedge::layer_neatline(data = .x, asp = asp, expand = TRUE, color = NA)
+      } else if (all(sapply(geom_type, function(x) {x %in% c("POINT", "MULTIPOINT")}))) {
+        fn <- ~ ggplot2::ggplot() +
+          overedge::layer_location_data(
+            data = .x,
+            mapping = mapping,
+            color = color,
+            size = size,
+            ...
+          ) +
+          overedge::layer_neatline(data = .x, asp = asp, expand = TRUE, color = NA)
+      } else if (all(sapply(geom_type, function(x) {x %in% c("LINESTRING", "MULTILINESTRING")}))) {
+        fn <- ~ ggplot2::ggplot() +
+          overedge::layer_location_data(
+            data = .x,
+            mapping = mapping,
+            color = color,
+            size = size,
+            linetype = linetype,
+            ...
+          ) +
+          overedge::layer_neatline(data = .x, asp = asp, expand = TRUE, color = NA)
+      }
+    }
+
+    fn <- rlang::as_function(fn)
+
+    gt_map_col <- purrr::map(
+      sf_nested$data,
+      ~ fn(.x)
+    )
+
+    gt::text_transform(
+      data = gt_object,
+      locations = gt::cells_body(columns = tidyselect::all_of(geom_col)),
+      fn = function(x) {
+        purrr::map(
+          gt_map_col,
+          ~ gt::ggplot_image(
+            plot_object = .x,
+            height = height,
+            aspect_ratio = asp
+          )
+        )
+      }
+    )
+  }
--- R/tbl_photo_key.R
+++ R/tbl_photo_key.R
@@ -1,96 +1,116 @@
-
-#' Create a numbered photo key table
+#' Create numbered photo key as a `gt` table
+#'
+#' Designed to work with the `overedge::read_sf_exif` function. More information on this approach here <https://elipousson.github.io/posts/2021-03-06-creating-a-key-map-for-photographs-with-r/>
+#'
+#' Function naming convention inspired by the {gtsummary} package <https://www.danieldsjoberg.com/gtsummary/index.html>
 #'
 #' @param data sf object or data frame with photo column containing the file
-#'   path or url for photos
-#' @param img_width Image width.
-#' @param img_height Image height.
+#'   path or url for photos.
+#' @param height The height in pixels of image in table.
+#' @param asp Aspect ratio of image (width/height)
 #' @param photo_col Photo path/url column name.
-#' @param title_col Photo title/description column name.
+#' @param title_col Title/description column name.
+#' @param title_size Title text size, Default: 'large'
+#' @param title_align Title tex alignment, Default: 'right'
+#' @param number If `TRUE`, add a number column that will by 20% of the width of
+#'   the photo column; defaults to `FALSE`. There may be a name conflict if the
+#'   dataframe already contains a column named "rowid".
+#' @param orientation Orientation to filter by if data contains image_width and
+#'   image_height columns.
+#' @md
 #' @export
-#' @importFrom overedge check_sf
+#' @importFrom overedge check_sf get_asp
 #' @importFrom sf st_drop_geometry
-#' @importFrom dplyr mutate row_number case_when filter
-#' @importFrom gt gt cols_width px tab_style cell_text cells_body opt_all_caps tab_options text_transform web_image local_image
-#' @importFrom tidyselect all_of
+#' @importFrom dplyr mutate case_when filter select row_number
+#' @importFrom usethis ui_stop
+#' @importFrom tidyselect everything all_of
+#' @importFrom gt gt cols_width px tab_style cell_text cells_body text_transform web_image local_image
 #' @importFrom purrr map
 tbl_photo_key <- function(data,
-                          img_width = 320,
+                          height = 240,
                           asp = 0.75,
                           photo_col = "photo",
                           title_col = "title",
+                          title_size = "large",
+                          title_align = "right",
                           number = FALSE,
                           orientation = NULL) {
   if (overedge::check_sf(data)) {
     data <- sf::st_drop_geometry(data)
   }
 
-  if (number) {
-    data <- data |>
-      dplyr::mutate(
-        number = dplyr::row_number(),
-        .before = .data[[photo_col]]
-      )
+  if (!is.null(asp)) {
+    asp <- overedge::get_asp(asp)
+  } else {
+    asp <- 0.75
   }
 
   if (!is.null(orientation)) {
-    img_orient <- orientation
+    image_orientation <- orientation
 
-    data <- data |>
-      dplyr::mutate(
-        orientation = dplyr::case_when(
-          (image_width / image_height) > 1 ~ "landscape",
-          (image_width / image_height) < 1 ~ "portrait",
-          (image_width / image_height) == 1 ~ "square"
-        ),
-        .after = .data[[photo_col]]
-      ) |>
-      dplyr::filter(.data$orientation %in% img_orient)
-
-  }
-
-  tbl <- data |>
-    gt::gt()
-
-    if (number) {
-      tbl <- tbl |>
-        gt::cols_width(
-          tidyselect::all_of("number") ~ gt::px(img_width * 0.2)
-        ) |>
-        gt::tab_style(
-          style = gt::cell_text(
-            weight = "bolder",
-            size = "xlarge",
-            v_align = "top",
-            align = "center"
+    if (all(c("image_height", "image_width") %in% names(data))) {
+      data <- data %>%
+        dplyr::mutate(
+          orientation = dplyr::case_when(
+            (image_width / image_height) > 1 ~ "landscape",
+            (image_width / image_height) < 1 ~ "portrait",
+            (image_width / image_height) == 1 ~ "square"
           ),
-          locations = gt::cells_body(columns = tidyselect::all_of("number"))
+          .after = .data[[photo_col]]
         )
+    } else if (!("orientation" %in% names(data))) {
+      usethis::ui_stop("Filtering images by orientation requires either an image_width and image_height column or an orientation column in the provided dataframe.")
     }
 
-    tbl <- tbl |>
-      gt::cols_width(
-        tidyselect::all_of(photo_col) ~ gt::px(img_width)
-      )
+    data <- data %>%
+      dplyr::filter(.data$orientation %in% image_orientation) %>%
+      dplyr::select(-c(image_width, image_height, orientation))
+  }
 
-  if (title_col %in% names(data)) {
-    tbl <- tbl |>
+  if (number) {
+    data <-
+      dplyr::mutate(
+        data,
+        rowid = dplyr::row_number(),
+        .before = tidyselect::everything()
+      )
+  }
+
+  tbl <- gt::gt(data)
+
+  tbl <-
+    gt::cols_width(
+      tbl,
+      tidyselect::all_of(photo_col) ~ gt::px(height / asp)
+    )
+
+  if (number) {
+    tbl <- tbl %>%
+      gt::cols_width(
+        "rowid" ~ gt::px((height / asp) * 0.2)
+      ) %>%
       gt::tab_style(
         style = gt::cell_text(
-          align = "right",
-          size = "large",
+          weight = "bolder",
+          size = "xlarge",
+          v_align = "top",
+          align = "center"
+        ),
+        locations = gt::cells_body(columns = "rowid")
+      )
+  }
+
+  if (title_col %in% names(data)) {
+    tbl <- tbl %>%
+      gt::tab_style(
+        style = gt::cell_text(
+          align = title_align,
+          size = title_size,
           v_align = "top"
         ),
         locations = gt::cells_body(columns = tidyselect::all_of(title_col))
       )
   }
-
-  tbl <- tbl |>
-    gt::opt_all_caps() |>
-    gt::tab_options(
-      table.width = img_width * 2,
-      heading.title.font.weight = "bolder"
-    )
 
   check_photo_url <-
     suppressWarnings(
@@ -98,21 +118,22 @@ tbl_photo_key <- function(data,
     )
 
   if (check_photo_url) {
-    tbl <- tbl |>
+    tbl <-
       gt::text_transform(
-        locations = tidyselect::all_of(photo_col),
-        fn = function(photo) {
-          purrr::map(photo, ~ gt::web_image(.x, height = img_width * asp))
+        data = tbl,
+        locations = gt::cells_body(columns = tidyselect::all_of(photo_col)),
+        fn = function(url) {
+          purrr::map(url, ~ gt::web_image(.x, height = height / asp))
         }
       )
   } else {
     tbl <- tbl |>
-    gt::text_transform(
-      locations = gt::cells_body(tidyselect::all_of(photo_col)),
-      fn = function(x) {
-        purrr::map(x, ~ gt::local_image(.x, height = img_width * asp))
-      }
-    )
+      gt::text_transform(
+        locations = gt::cells_body(photo_col),
+        fn = function(x) {
+          purrr::map(x, ~ gt::local_image(.x, height = height / asp))
+        }
+      )
   }
 
   return(tbl)
