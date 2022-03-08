39f50da5e266233fadadbbdc32fb28c205655e24
Eli Pousson <eli.pousson@gmail.com>
2022-03-08 08:40:07 -0500

    fix: correct issue w/ group_by_col
    - fix: correct issue w/ group_by_col utility function; group_by needed  .data$groupname_col data masking not {{ groupname_col }}

--- R/utils.R
+++ R/utils.R
@@ -10,10 +10,18 @@ check_url <- function(x) {
 
 #' Group by column name if present
 #'
+#' Group is groupname_col is not NULL and is present in data. Stop if name is not present and data is not NULL.
+#'
+#' @param data Data to group
+#' @param groupname_col Column name to group by, Default: NULL
 #' @noRd
+#' @importFrom dplyr group_by
+#' @importFrom usethis ui_stop
 group_by_col <- function(data, groupname_col = NULL) {
   if (!is.null(groupname_col) && (groupname_col %in% names(data))) {
-    dplyr::group_by(data, {{ groupname_col }})
+    dplyr::group_by(data, .data$groupname_col)
+  } else if (!is.null(data) && !(groupname_col %in% names(data))) {
+    usethis::ui_stop("The provided data does not have a column matching this groupname_col ({usethis::ui_value(groupname_col)}).")
   } else {
     data
   }
@@ -22,4 +30,3 @@ group_by_col <- function(data, groupname_col = NULL) {
 utils::globalVariables(c(
   ".data", " .data", "image_height", "image_width", "numbered"
 ))
-
